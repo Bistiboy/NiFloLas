757fc7f6fe1c922e3b807687bd68da372bc4d0da
Florian Taurand <florian042011@hotmail.fr>
2017-11-15 23:29:39 +0100

    Update AddEmploye.java

--- NiFloLas-master/views/AddEmploye.java
+++ NiFloLas-master/views/AddEmploye.java
@@ -1,11 +1,47 @@
 package views;
 
+import service.Personnel;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
-//classe de création d'employé
-public class AddEmploye extends JPanel {
-    //vide
+public class AddEmploye extends JPanel{
+    public Personnel personnel;
+    private JFrame gui;
+    public JPanel panelDashboard;
+
+    //constructeur de la vue
 
 
-    //vide aussi
+    public void refreshPageAdd(){
+        this.gui.getContentPane().removeAll();
+        this.gui.getContentPane().add(this);
+        this.gui.revalidate();
+        this.gui.repaint();
+    }
+
+    private void retour(ActionEvent actionEvent) {
+        // gui.setContentPane();
+    }
+
+    public void run(JFrame gui, Personnel personnel) {
+        this.personnel = personnel;
+        this.gui = gui;
+
+
+        JLabel title = new JLabel("Ajouter un employÃ©");
+        JButton retourMenu = new JButton("Retour");
+
+        retourMenu.addActionListener(e -> {
+            this.gui.getContentPane().removeAll();
+            this.gui.getContentPane().add(this.panelDashboard);
+            this.gui.revalidate();
+            this.gui.repaint();
+        });
+
+
+        this.add(title); //ajout du titre
+        this.add(retourMenu); // bouton retour
+
+    }
 }
