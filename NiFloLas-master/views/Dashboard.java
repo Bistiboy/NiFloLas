9c2ddcd2296e6cab0865b7318ba636a2d17c85b4
Florian Taurand <florian042011@hotmail.fr>
2017-11-15 23:28:21 +0100

    Update Dashboard.java

--- NiFloLas-master/views/Dashboard.java
+++ NiFloLas-master/views/Dashboard.java
@@ -1,3 +1,4 @@
+
 package views;
 
 import domain.*;
@@ -8,64 +9,103 @@ import service.*;
 import javax.swing.*;
 import java.awt.*;
 import views.AddEmploye;
-
 import static javax.swing.SwingUtilities.invokeLater;
 
 
 public class Dashboard {
+    private JPanel panelAdd;
+    private JPanel panelView;
+    private JPanel panelMain = new JPanel();
+    private JFrame frame;
+
+    public Dashboard(JFrame frame){
+        this.frame = frame;
+
+    }
+
+
+
+
     public static void main(String[] args){
         invokeLater(Dashboard::window);
     }
 
 
-    private static void window(){
-        JFrame frame = new JFrame("Roger");
+    public static void window() {
+        Dashboard app = new Dashboard(new JFrame("Programme Java"));
+        app.run(new AddEmploye(), new Affichage());
+    }
+
+
+
+
+
+    public void run(AddEmploye addEmploye, Affichage affichage){
         Personnel personnel = new Personnel();
+
         personnel.ajouterEmploye(new Vendeur("Florian","Taurand",2,"2016",12));
         personnel.ajouterEmploye(new Vendeur("Florian","Taurand2",2,"2016",12));
         personnel.ajouterEmploye(new Vendeur("Florian","Taurand3",2,"2016",12));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panelDashboard = new JPanel();
-        JPanel panelAdd = new JPanel();
-        JPanel panelView = new JPanel();
-
-
-        JButton buttonAdd = new JButton("Ajouter des employées");
-        buttonAdd.addActionListener(e -> System.out.println("clicked"));
-
-        JButton buttonView = new JButton("Voir les employées");
-        buttonView.addActionListener(e -> new Affichage(frame, personnel)); // bouton pour afficher la liste
-
-        frame.getContentPane().setLayout(new FlowLayout());
-        frame.getContentPane().add(buttonAdd);
-        frame.getContentPane().add(buttonView);
+        //addEmploye.run();
+        affichage.run(this.frame, personnel);
+        addEmploye.run(this.frame, personnel);
 
 
-        panelDashboard.add(buttonAdd, BorderLayout.CENTER);
-        panelDashboard.add(buttonView, BorderLayout.CENTER);
 
 
-        frame.getContentPane().add(panelDashboard);
+
+
+
+
+        JButton buttonAdd = new JButton("Ajouter des employÃ©es");
+        buttonAdd.addActionListener(e -> addEmploye.refreshPageAdd());
+        //buttonAdd.addActionListener(e -> new Affichage(this.frame, AddEmploye));
+
+        JButton buttonView = new JButton("Voir les employÃ©es");
+        buttonView.addActionListener(e -> affichage.refreshPage()); // bouton pour afficher la liste
+
+        this.panelMain.setLayout(new FlowLayout());
+        this.panelMain.add(buttonAdd);
+        this.panelMain.add(buttonView);
+
+        affichage.panelDashboard = this.panelMain;
+        addEmploye.panelDashboard = this.panelMain;
+
+
+        //panelDashboard.add(buttonAdd, BorderLayout.CENTER);
+        //panelDashboard.add(buttonView, BorderLayout.CENTER);
+
+
+        //frame.getContentPane().add(panelDashboard);
 
 
         //frame.pack();
-        frame.setSize(800, 500);
-        //frame.setUndecorated(true);
+        this.frame.setSize(800, 500);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setLocation(dim.width/2 - frame.getWidth()/2, dim.height/2 - frame.getHeight()/2);
-        frame.setVisible(true);
+        this.frame.setLocation(dim.width/2 - this.frame.getWidth()/2, dim.height/2 - this.frame.getHeight()/2);
+        this.frame.setVisible(true);
+
+        this.frame.getContentPane().removeAll();
+        this.frame.getContentPane().add(this.panelMain);
+        this.frame.revalidate();
+        this.frame.repaint();
+
     }
 
-    public static void run(){
+
+
+    private static void run2(){
         JFrame frame = new JFrame("Tableau");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JTable table = new JTable(new Object[][]{
                 {"Nom","Valeur 1"},
-                {"Prénom", "Valeur 2"},
+                {"PrÃ©nom", "Valeur 2"},
                 {"Age", "Valeur 3"},
-                {"Date d'entrée", "Valeur 4"},
+                {"Date d'entrÃ©e", "Valeur 4"},
                 {"Type", "Valeur 5"},
                 {"Salaire", "Valeur 6"}
         }, new String[]{"Informations", "Valeurs"});
